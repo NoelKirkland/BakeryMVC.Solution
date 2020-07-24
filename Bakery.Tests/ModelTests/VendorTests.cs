f575baadfbce4045a7285b9185768bc6f4020540
Noel Kirkland <noelkirkland@gmail.com>
2020-07-24 15:11:56 -0700

    construct and fail second test for spec 1

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,19 @@ namespace Bakery.TestTools
       Vendor newVendor = new Vendor("Grand Central", "best source of scones and danishes in Portland");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void VendorConstructor_ReturnObjectValues_String()
+    {
+      string name = "Grand Central";
+      string description = "best source of scones and danishes in Portland";
+      Vendor newVendor = new Vendor(name, description);
+
+      string resultName = newVendor.Name;
+      string resultDescription = newVendor.Description;
+
+      Assert.AreEqual("not name", resultName);
+      Assert.AreEqual("not description", resultDescription);
+    }
   }
 }
