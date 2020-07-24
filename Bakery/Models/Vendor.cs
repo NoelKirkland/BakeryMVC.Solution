4e8a20b0af0cbce513b40a03da9d0ac2472619ca
Noel Kirkland <noelkirkland@gmail.com>
2020-07-24 16:09:55 -0700

    add a find method to my Vendor class and fail test for spec 8

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -6,6 +6,7 @@ namespace Bakery.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Id { get; }
     public static List<Vendor> _instances = new List<Vendor> {};
 
     public Vendor(string name, string description)
@@ -13,6 +14,7 @@ namespace Bakery.Models
       Name = name;
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static void ClearAll()
@@ -23,6 +25,11 @@ namespace Bakery.Models
     public static List<Vendor> GetAll()
     {
       return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
