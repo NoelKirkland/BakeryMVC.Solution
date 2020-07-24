1da5699dbb57d15551efd4f5408634527bbabd25
Noel Kirkland <noelkirkland@gmail.com>
2020-07-24 13:22:55 -0700

    construct and fail second test for spec one

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -14,6 +14,26 @@ namespace Bakery.Tests
       Order newOrder = new Order("scones", "7/24/2020", "we ordered 50 scones but we might need more soon", 110.00);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void OrderConstructor_ReturnValues_StringAndDouble()
+    {
+      string title = "scones";
+      string date = "7/24/2020";
+      string description = "we ordered 50 scones but we might need more soon";
+      double price = 110.00;
+      Order newOrder = new Order(title, date, description, price);
+
+      string resultTitle = newOrder.Title;
+      string resultDate = newOrder.Date;
+      string resultDescription = newOrder.Description;
+      double resultPrice = newOrder.Price;
+
+      Assert.AreEqual("not this 1", resultTitle);
+      Assert.AreEqual("not this 2", resultDate);
+      Assert.AreEqual("not this 3", resultDescription);
+      Assert.AreEqual("not this 4", resultPrice);
+    }
   }
 }
 
