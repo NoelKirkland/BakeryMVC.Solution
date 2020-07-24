aa27483aaa166cccb36cabd0785d19ddcd95a4ac
Noel Kirkland <noelkirkland@gmail.com>
2020-07-24 15:25:48 -0700

    add a vendor list field to the Vendor object and implement the Add method to the Vendor constructor so that it add each instance of the Vendor object to this list

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -4,11 +4,23 @@ namespace Bakery.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+    public static List<Vendor> _instances = new List<Vendor> {};
 
     public Vendor(string name, string description)
     {
       Name = name;
       Description = description;
+      _instances.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
     }
   }
 }
